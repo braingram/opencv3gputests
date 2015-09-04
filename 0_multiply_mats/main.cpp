deec3b7f6b8688306338515e5e371ef6d70c01d7
brett <brettgraham@gmail.com>
2015-09-03 21:26:08 -0400

    adding gpu multiply and timing of upload and download

--- 0_multiply_mats/main.cpp
+++ 0_multiply_mats/main.cpp
@@ -2,53 +2,143 @@
 #include <sys/time.h>
 #include <iostream>
 #include "opencv2/core/core.hpp"
+#include "opencv2/core/cuda.hpp"
+#include "opencv2/cudaarithm.hpp"
 
 #define N 10
 
-using namespace std;
-using namespace cv;
 
-void time_cpu() {
-    struct timeval start_time, end_time;
-    struct timeval delta_times[N];
-    double mean_delta_time;
-    // = new timeval[N];
+//using namespace std;
+//using namespace cv;
 
-    // make mat on cpu
-    Mat a(2048, 2048, CV_32F, Scalar(0.));
-    Mat b(2048, 2048, CV_32F, Scalar(0.));
-    // cout << "Made mat of " << a.size() << endl;
-    randu(a, Scalar(0), Scalar(100));
-    randu(b, Scalar(0), Scalar(100));
-    // cout << "filled with random values" << endl;
 
-    double min_value;
-    double max_value;
+struct timeval start_time, end_time;
+struct timeval delta_times[N];
+double mean_delta_time;
 
-    minMaxLoc(a, &min_value, &max_value);
-    // cout << "a: min: " << min_value << " max: " << max_value << endl;
-    minMaxLoc(b, &min_value, &max_value);
-    // cout << "b: min: " << min_value << " max: " << max_value << endl;
+cv::Mat a(2048, 2048, CV_32F, cv::Scalar(0.));
+cv::Mat b(2048, 2048, CV_32F, cv::Scalar(0.));
 
-    // cout << "multiply a * b " << endl;
+
+double time_gpu_upload() {
+    cv::randu(a, cv::Scalar(0), cv::Scalar(100));
+    
+    cv::cuda::GpuMat ga(2048, 2048, CV_32F);
+
+
+    // transfer both the gpu
     for (int i=0; i < N; i++) {
         gettimeofday(&start_time, NULL);
 
-        multiply(a, b, b);
+        ga.upload(a);
 
         gettimeofday(&end_time, NULL);
         timersub(&end_time, &start_time, &delta_times[i]);
-        cout << "cpu: multiply took: " << (delta_times[i].tv_usec + 1000000 * delta_times[i].tv_sec) / 1000. << endl;
+        std::cout << "gpu: upload took: " << (delta_times[i].tv_usec + 1000000 * delta_times[i].tv_sec) / 1000. << std::endl;
+        mean_delta_time += (delta_times[i].tv_usec + 1000000 * delta_times[i].tv_sec) / 1000.;
+
+    }
+
+    ga.release();
+
+    mean_delta_time /= N;
+    std::cout << "gpu: mean time: " << mean_delta_time << std::endl;
+    return mean_delta_time;
+}
+
+
+double time_gpu_download() {
+    cv::randu(a, cv::Scalar(0), cv::Scalar(100));
+    cv::randu(b, cv::Scalar(0), cv::Scalar(100));
+    
+    cv::cuda::GpuMat ga(2048, 2048, CV_32F);
+    cv::cuda::GpuMat gb(2048, 2048, CV_32F);
+
+    ga.upload(a);
+    gb.upload(b);
+
+    cv::cuda::multiply(ga, gb, gb);
+
+    ga.release();
+
+    // transfer both the gpu
+    for (int i=0; i < N; i++) {
+        gettimeofday(&start_time, NULL);
+
+        gb.download(b);
+
+        gettimeofday(&end_time, NULL);
+        timersub(&end_time, &start_time, &delta_times[i]);
+        std::cout << "gpu: upload took: " << (delta_times[i].tv_usec + 1000000 * delta_times[i].tv_sec) / 1000. << std::endl;
+        mean_delta_time += (delta_times[i].tv_usec + 1000000 * delta_times[i].tv_sec) / 1000.;
+
+    }
+
+    gb.release();
+
+    mean_delta_time /= N;
+    std::cout << "gpu: mean time: " << mean_delta_time << std::endl;
+    return mean_delta_time;
+}
+
+
+double time_gpu_multiply() {
+    cv::randu(a, cv::Scalar(0), cv::Scalar(100));
+    cv::randu(b, cv::Scalar(0), cv::Scalar(100));
+    
+    cv::cuda::GpuMat ga(2048, 2048, CV_32F);
+    cv::cuda::GpuMat gb(2048, 2048, CV_32F);
+
+    ga.upload(a);
+    gb.upload(b);
+
+    // transfer both the gpu
+    for (int i=0; i < N; i++) {
+        gettimeofday(&start_time, NULL);
+
+        cv::cuda::multiply(ga, gb, gb);
+
+        gettimeofday(&end_time, NULL);
+        timersub(&end_time, &start_time, &delta_times[i]);
+        std::cout << "gpu: multiply took: " << (delta_times[i].tv_usec + 1000000 * delta_times[i].tv_sec) / 1000. << std::endl;
+        mean_delta_time += (delta_times[i].tv_usec + 1000000 * delta_times[i].tv_sec) / 1000.;
+
+    }
+
+    mean_delta_time /= N;
+    std::cout << "gpu: mean time: " << mean_delta_time << std::endl;
+    return mean_delta_time;
+}
+
+
+float time_cpu_multiply() {
+    cv::randu(a, cv::Scalar(0), cv::Scalar(100));
+    cv::randu(b, cv::Scalar(0), cv::Scalar(100));
+
+    for (int i=0; i < N; i++) {
+        gettimeofday(&start_time, NULL);
+
+        cv::multiply(a, b, b);
+
+        gettimeofday(&end_time, NULL);
+        timersub(&end_time, &start_time, &delta_times[i]);
+        std::cout << "cpu: multiply took: " << (delta_times[i].tv_usec + 1000000 * delta_times[i].tv_sec) / 1000. << std::endl;
         mean_delta_time += (delta_times[i].tv_usec + 1000000 * delta_times[i].tv_sec) / 1000.;
     }
 
     mean_delta_time /= N;
-    minMaxLoc(b, &min_value, &max_value);
-    // cout << "b: min: " << min_value << " max: " << max_value << endl;
-
-    cout << "cpu: mean time: " << mean_delta_time << endl;
+    std::cout << "cpu: mean time: " << mean_delta_time << std::endl;
+    return mean_delta_time;
 }
 
 int main() {
-   time_cpu();
+   double cpu_multiply = time_cpu_multiply();
+   double gpu_upload = time_gpu_upload();
+   double gpu_multiply = time_gpu_multiply();
+   double gpu_download = time_gpu_download();
+   std::cout << std::endl;
+   std::cout << "CPU multiply: " << cpu_multiply << std::endl;
+   std::cout << "GPU upload  : " << gpu_upload << std::endl;
+   std::cout << "GPU download: " << gpu_download << std::endl;
+   std::cout << "GPU multiply: " << gpu_multiply << std::endl;
 }
