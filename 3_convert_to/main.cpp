586e078de8a85d0dcb68886bee91a2127efcb9ef
brett <brettgraham@gmail.com>
2015-09-04 15:05:46 -0400

    adding gitignore

--- 3_convert_to/main.cpp
+++ 3_convert_to/main.cpp
@@ -4,6 +4,8 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
+#define N 10
+
 cv::Mat im;
 cv::Mat fim = cv::Mat(2048, 2048, CV_32F);
 
@@ -11,11 +13,15 @@ struct timeval start, end, delta;
 
 int main() {
     im = cv::imread("../data/0.tif");
-    gettimeofday(&start, NULL);
-    im.convertTo(fim, CV_32F);
-    gettimeofday(&end, NULL);
-    timersub(&end, &start, &delta);
-    std::cout << (delta.tv_usec + 1000000 * delta.tv_sec) / 1000. << std::endl;
+
+    for (int i=0; i < N; i++) {
+        gettimeofday(&start, NULL);
+        im.convertTo(fim, CV_32F);
+        gettimeofday(&end, NULL);
+        timersub(&end, &start, &delta);
+        std::cout << (delta.tv_usec + 1000000 * delta.tv_sec) / 1000. << std::endl;
+    }
+
     im.release();
     fim.release();
 }
